c4df137c339914bbc81765e680f040ea11c3c44c
Verse <ywchow@umich.edu>
2017-05-29 23:36:07 -0400

    UPDATE: Usage for data_collection.sh

--- data_collection.sh
+++ data_collection.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Usage: please specify oneshot dataset directory to train and test, and directory name to save the collection data
-#like: ./data_collection.sh ./table/table_9 progress
+#also you could then specify name of jpg and xml to save and label to save
+#like: ./data_collection.sh ./table/table_9 progress table_9_ table
 
 set -x
 set -e
@@ -10,7 +11,10 @@ if [ $# -eq 0 ]
 else
 	dataset=$1
 	saved_name=$2
-	python main.py --train False --train_test_dataset ${dataset} --dataset ${saved_name}
+	jpg_xml_name=$3
+	label=$4
+	python main.py --train False --train_test_dataset ${dataset} --dataset ${saved_name}\
+	--saved_name ${jpg_xml_name} --label ${label}
 fi
 
 set +x
